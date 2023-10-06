574fb67e124b59b9d9338150af63be37a7095bd3
BT_2N2_12 <sample@bths.edu>
2023-10-06 14:01:20 -0400

    2

--- src/LabRunner.java
+++ src/LabRunner.java
@@ -11,12 +11,23 @@ public class LabRunner {
         System.out.println("Enter next word: ");
         String word2 = scan.nextLine();
 
-        if ((word1.compareTo(word2))>0) {
+        if ((word1.compareTo(word2)) > 0) {
             pointTotal += 2;
             System.out.println("+2 points: current word alphabetically after; SCORE: " + pointTotal);
-            }else {
-            System.out.print("");
-            }
-        if ((word1))
+        } else {
+            pointTotal -= 5;
+            System.out.println("-5 points: current word alphabetically before; SCORE: " + pointTotal);
         }
+
+        if ((word1.equals(word2))) {
+            pointTotal -= 10;
+            System.out.println("-10 points: current word is the same as before; SCORE: " + pointTotal);
+        } else {
+            System.out.print("");
+        }
+
+        String word1First2Letters = word1.substring(0,3);
+        String word2Last2Letters = word2length
+        if ()
     }
+}
